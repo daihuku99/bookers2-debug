387b379912b5cb78f67cf108f79762c0133504fd
daihuku99 <dai9huku@gmail.com>
2020-04-06 15:41:06 +0900

    add thanksmailer

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'home#top'
   get 'home/about' => 'home#about', as: :about
-  devise_for :users
-  # devise_for :users, controllers: {
-  # 	sessions: 'users/sessions',
-  # 	registrations: 'users/registrations'
-  # }
+  devise_for :users, controllers: {
+  	sessions: 'users/sessions',
+  	registrations: 'users/registrations'
+  }
   resources :users, only: [:show,:index,:edit,:update]
 
   resources :books do
